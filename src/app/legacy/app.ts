e0b60d8fb7edd6202e2a722bf8b15c8bee18fb87
amindunited <buckley.robin@gmail.com>
2017-06-25 18:40:47 +1000

    feat(import-es5): Import es5 app and load to prove functionality

--- src/app/legacy/app.ts
+++ src/app/legacy/app.ts
@@ -3,10 +3,18 @@ declare var angular:any;
 import { downgradeComponent } from '@angular/upgrade/static';
 import { AppComponent } from '../app.component';
 
-export const NgOneModule = angular.module('ngOneModule', []);
+//import * as SimpleEs5 from 'simple-ng1.5-es5/app/app';
+
+
+export const NgOneModule = angular.module('ngOneModule', ['oldNG1']);
 
 NgOneModule.component('ngOneComponent', {
-  template: `<div>NgOneAppModule!</div><ng-two-app></ng-two-app>`
+  template: `<div>NgOneAppModule!</div>
+              <md-toolbar layout="row" class="md-toolbar-tools">
+                <h1>International League of Villains</h1>
+              </md-toolbar>
+              <old-select-wrapper></old-select-wrapper>
+          <ng-two-app></ng-two-app>`
 })
 
 NgOneModule

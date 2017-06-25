63a404eea73a62ec193c8e8d756d3763622916bb
amindunited <buckley.robin@gmail.com>
2017-06-25 17:29:47 +1000

    chore(setup): Bootstrap the legacy module

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { UpgradeModule } from '@angular/upgrade/static';
+
+import { NgOneModule } from './app/legacy/app';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -8,4 +11,7 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule).then(platformRef => {
+  const upgrade = platformRef.injector.get(UpgradeModule) as UpgradeModule;
+  upgrade.bootstrap(document.body, [NgOneModule.name], {strictDi: true});
+});
